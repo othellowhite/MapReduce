d33354497d133741f796f551b4801165500acce6
Soyeong Park <soyeong@Soyeong-ui-MacBook-Air.local>
2015-07-24 23:59:55 -0700

    Implement a source code to calculate average of age based on location's information
    Writer: Soyeong Park
    
    This source code was made to understand MapReduce and we implement to
    calculate average of age based on location.
    
    So to speak, This program is a source to calculate average of age based
    on location’s information after xml is parsed. And I used
    CountLocation’s program source code.

---
+++ src_soyeong/AverageLocation/src/test/java/edu/uci/soyeongp/AverageLocation/AppTest.java
@@ -0,0 +1,18 @@
+package edu.uci.soyeongp.AverageLocation;
+
+import org.junit.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void sumTest() {
+//		int a = 1;
+//		int b = 2;
+//		int expected = 3;
+//		int result = CountLocation.sum(a, b);
+//		
+//		assertEquals(expected, result);
+	}
+}
